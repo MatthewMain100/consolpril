c5f6aa3f9369dc4812da95d5b6245e8322a997ed
Maks <maks.vympel@mail.ru>
2021-01-28 20:46:47 +0300

    sportproekt

--- fike44.js
+++ fike44.js
@@ -1,23 +1,34 @@
-'use strict';
-const box=document.getElementById('box'),
-     btns= document.getElementsByTagName('button'),
-     circles=document.getElementsByClassName('circle'),
-     hearts=document.querySelectorAll('.heart'),
-     oneHeart=document.querySelectorAll('.heart'),
-     wrapper=  document.querySelector('wrapper');
-     //console.dir(box);
-     box.style.backgroundColor='pink';
-     btns[1].style.backgroundColor='red';
-     hearts.forEach(item=>{
-         item.style.backgroundColor='pink';
-     });
-      const div=document.createElement('div');
-     div.classList.add('black');
-      document.body.append(div);
-     // wrapper.append(div);
-    //    wrapper.prepend(div);
-    // hearts[0].after(div);
-    // circles[0].remove();
-     hearts[0].replaceWith(circles[0]);
-     div.innerHTML='hello friend';
-    div.insertAdjacentHTML("afterrbegin",'<h2>Hello </h2>');
+window.addEventListener('DOMContentLoaded', function() {
+const btn0d=document.querySelector(' .btn');
+btn0d.addEventListener('click',()=>{
+alert('матвей писькогрыз');
+});
+const btn=document.querySelector('.knopka');
+btn.addEventListener('click',()=>{
+});
+ const knopka=document.querySelector('.knopka');
+ const knopka1=document.querySelector('.knopka1');
+ const knopka2=document.querySelector('.knopka2');
+ const knopka3=document.querySelector('.knopka3');
+ const pullup=document.querySelector('.pullup');
+ const pullup1=document.querySelector('.pullup1');
+ const pullup2=document.querySelector('.pullup2');
+function bords(){
+    pullup.style.display='none';
+    pullup1.style.display='none';
+    pullup2.style.display='none';
+}
+bords();
+knopka.addEventListener('click',()=>{
+    pullup1.style.display='block';
+});
+knopka3.addEventListener('click',()=>{
+    pullup1.style.display='block';
+});
+knopka2.addEventListener('click',()=>{
+    pullup.style.display='block';
+});
+knopka1.addEventListener('click',()=>{
+    pullup2.style.display='block';
+});
+});
